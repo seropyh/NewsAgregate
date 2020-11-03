721b95d3d5a5f3f28b4100ca39f1b647b6eba384
Geliostigma <zharikovjura@gmail.com>
2020-11-03 10:56:23 +0300

    проект для теста

---
+++ vayprog/vayprog/src/main/java/com/example/vayprog/MainController.java
@@ -0,0 +1,34 @@
+package com.example.vayprog;
+
+
+
+import com.example.vayprog.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "index";
+    }
+
+
+
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        model.addAttribute("greeting",new Greeting());
+        return "blog";
+    }
+
+
+    @GetMapping("/result")
+    public String result(Model model) {
+        model.addAttribute("greeting",new Greeting());
+        return "result";
+    }
+
+}
